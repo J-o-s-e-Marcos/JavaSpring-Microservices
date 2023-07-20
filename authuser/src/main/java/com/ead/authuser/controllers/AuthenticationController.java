eb1a2e5dcd05a9c2c83838440fe7d1391376d510
Marcos <marcos1jose@live.com>
2023-07-20 15:23:10 -0300

    Validacao customizada em authuser

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -29,7 +30,7 @@ public class AuthenticationController {
 
         @PostMapping("/signup")//mapeia a URL /auth/signup para o método registerUser(), que manipula as
         // solicitações HTTP POST.
-        public ResponseEntity<Object> registerUser(@RequestBody
+        public ResponseEntity<Object> registerUser(@RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
                                                            @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto){
           if(userService.existsByUsername(userDto.getUsername())){
               return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: Username is Alredy Taken!");
